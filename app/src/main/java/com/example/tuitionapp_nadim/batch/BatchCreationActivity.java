9f5779a6816ecd584d78cc5659e97301c2af7cbe
Nadim1028 <bsse1028@iit.du.ac.bd>
2020-05-19 07:12:20 +0600

    MessageBox few problems solved

---
+++ app/src/main/java/com/example/tuitionapp_nadim/batch/BatchCreationActivity.java
@@ -0,0 +1,191 @@
+package com.example.tuitionapp_nadim.batch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.tuitionapp_nadim.group.GroupHomePageActivity;
+import com.example.tuitionapp_nadim.R;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class BatchCreationActivity extends AppCompatActivity {
+    private DatabaseReference myRefBatchInfo ;
+
+    private Button createNewBatchButton;
+
+    private EditText [] scheduleEditText ;
+    private TextInputEditText batchNameEditText, paymentEditText, noOfAvailableSeatEditText, extraInfoEditText ;
+
+    private String batchNameStr, paymentStr, noOfSeatAvailability, extraInfo ;
+    private ArrayList<String> schedule ;
+
+    private String user, userEmail, groupID;
+    private ArrayList<String> userInfo ;
+
+    private MaterialToolbar materialToolbar ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_batch_creation);
+
+        Intent intent = getIntent() ;
+        user = intent.getStringExtra("user") ;
+        groupID = intent.getStringExtra("groupID") ;
+
+        if(user.equals("tutor")){
+            userInfo = intent.getStringArrayListExtra("userInfo") ;
+        }
+        else{
+            userEmail = intent.getStringExtra("userEmail") ;
+        }
+
+        myRefBatchInfo = FirebaseDatabase.getInstance().getReference("Batch");
+
+        createNewBatchButton = findViewById(R.id.createBatchButton) ;
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        schedule = new ArrayList<>() ;
+
+        batchNameEditText = findViewById(R.id.batchNameEditText) ;
+        noOfAvailableSeatEditText = findViewById(R.id.no_of_available_seat) ;
+        paymentEditText = findViewById(R.id.payment) ;
+        extraInfoEditText = findViewById(R.id.extra) ;
+
+        scheduleEditText = new EditText[32] ;
+
+        scheduleEditText[0] = findViewById(R.id.time1) ;
+        scheduleEditText[1] = findViewById(R.id.T11) ;
+        scheduleEditText[2] = findViewById(R.id.T12) ;
+        scheduleEditText[3] = findViewById(R.id.T13) ;
+        scheduleEditText[4] = findViewById(R.id.T14) ;
+        scheduleEditText[5] = findViewById(R.id.T15) ;
+        scheduleEditText[6] = findViewById(R.id.T16) ;
+        scheduleEditText[7] = findViewById(R.id.T17) ;
+        scheduleEditText[8] = findViewById(R.id.time2) ;
+        scheduleEditText[9] = findViewById(R.id.T21) ;
+        scheduleEditText[10] = findViewById(R.id.T22) ;
+        scheduleEditText[11] = findViewById(R.id.T23) ;
+        scheduleEditText[12] = findViewById(R.id.T24) ;
+        scheduleEditText[13] = findViewById(R.id.T25) ;
+        scheduleEditText[14] = findViewById(R.id.T26) ;
+        scheduleEditText[15] = findViewById(R.id.T27) ;
+        scheduleEditText[16] = findViewById(R.id.time3) ;
+        scheduleEditText[17] = findViewById(R.id.T31) ;
+        scheduleEditText[18] = findViewById(R.id.T32) ;
+        scheduleEditText[19] = findViewById(R.id.T33) ;
+        scheduleEditText[20] = findViewById(R.id.T34) ;
+        scheduleEditText[21] = findViewById(R.id.T35) ;
+        scheduleEditText[22] = findViewById(R.id.T36) ;
+        scheduleEditText[23] = findViewById(R.id.T37) ;
+
+        materialToolbar = findViewById(R.id.topAppBar) ;
+
+        materialToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToBackPageActivity();
+            }
+        });
+    }
+
+
+    public void createBatchCompletion(View view){
+        ArrayList<BatchScheduleInfo> batchScheduleInfoArrayList = new ArrayList<>();
+
+        batchNameStr = batchNameEditText.getText().toString().trim();
+        paymentStr = paymentEditText.getText().toString().trim() ;
+        noOfSeatAvailability = noOfAvailableSeatEditText.getText().toString() ;
+        extraInfo = extraInfoEditText.getText().toString() ;
+
+        if(batchNameStr.equals("")){
+            batchNameEditText.setError("this should not be empty");
+            return;
+        }
+
+        int flag = 0 ;
+        for(int i=1 ; i<8 ; i++){
+            schedule.add(scheduleEditText[i].getText().toString()) ;
+            if(!scheduleEditText[i].getText().toString().equals("")){
+                flag = 1 ;
+            }
+        }
+
+        String time = scheduleEditText[0].getText().toString() ;
+        BatchScheduleInfo batchScheduleInfo = new BatchScheduleInfo(time,schedule.get(0), schedule.get(1), schedule.get(2), schedule.get(3), schedule.get(4), schedule.get(5), schedule.get(6)) ;
+        batchScheduleInfoArrayList.add(batchScheduleInfo) ;
+        schedule.clear();
+
+        flag = 0 ;
+        for(int i=9 ; i<16 ; i++){
+            schedule.add(scheduleEditText[i].getText().toString()) ;
+            if(!scheduleEditText[i].getText().toString().equals("")){
+                flag = 1 ;
+            }
+        }
+
+        if(flag==1){
+            time = scheduleEditText[8].getText().toString() ;
+            batchScheduleInfo = new BatchScheduleInfo(time,schedule.get(0), schedule.get(1), schedule.get(2), schedule.get(3), schedule.get(4), schedule.get(5), schedule.get(6)) ;
+            batchScheduleInfoArrayList.add(batchScheduleInfo) ;
+        }
+
+        schedule.clear();
+        flag = 0 ;
+        for(int i=17 ; i<24 ; i++){
+            schedule.add(scheduleEditText[i].getText().toString()) ;
+            if(!scheduleEditText[i].getText().toString().equals("")){
+                flag = 1 ;
+            }
+        }
+
+        if(flag==1){
+            time = scheduleEditText[16].getText().toString() ;
+            batchScheduleInfo = new BatchScheduleInfo(time,schedule.get(0), schedule.get(1), schedule.get(2), schedule.get(3), schedule.get(4), schedule.get(5), schedule.get(6)) ;
+            batchScheduleInfoArrayList.add(batchScheduleInfo) ;
+        }
+
+        BatchInfo batchInfo = new BatchInfo(batchNameStr, noOfSeatAvailability, paymentStr, extraInfo, batchScheduleInfoArrayList , groupID) ;
+        myRefBatchInfo.push().setValue(batchInfo) ;
+        Intent intent = new Intent(this, GroupHomePageActivity.class);
+
+        if(user.equals("tutor")){
+            intent.putStringArrayListExtra("userInfo",userInfo) ;
+        }
+        intent.putExtra("user",user) ;
+        intent.putExtra("groupID" , groupID) ;
+        startActivity(intent);
+        finish();
+    }
+
+
+    public void goToBackPageActivity(){
+        Intent intent = new Intent(this, GroupHomePageActivity.class);
+        intent.putExtra("groupID",groupID) ;
+        if(user.equals("tutor")){
+            intent.putStringArrayListExtra("userInfo",userInfo) ;
+        }
+        intent.putExtra("user", user) ;
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed(){
+        goToBackPageActivity();
+    }
+}
---
+++ app/src/main/java/com/example/tuitionapp_nadim/candidate_tutor/TutorSignUpActivityStep1.java
@@ -0,0 +1,250 @@
+package com.example.tuitionapp_nadim.candidate_tutor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.os.Bundle;
+
+import com.example.tuitionapp_nadim.R;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class TutorSignUpActivityStep1 extends AppCompatActivity {
+
+    private EditText addressEditText , userNameEditText, mobileNumberEditText, edu_instituteNameEditText, attachedHallBox;
+    private TextView edu_tutorSubjectTextView, attachedHallTextView, mobileNumberTextView, userNameTextView ;
+
+    private Spinner genderBox, currentPositionBox, edu_instituteNameBox, edu_tutorSubjectBox ;
+
+    private DatabaseReference myRefCandidateTutor ;
+    private FirebaseUser firebaseUser;
+
+    private String userName="" , email="", mobileNumber="", gender="", areaAddress="", currentPosition="", edu_instituteName="", edu_tutorSubject="", attachedHall="" ;
+
+    private CandidateTutorInfo candidateTutorInfo,info;
+
+    private String type ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutor_sign_up_step1_new);
+        Intent intent = getIntent() ;
+        type = intent.getStringExtra("type");
+
+        userNameEditText = findViewById(R.id.userName);
+        userNameTextView = findViewById(R.id.userNameTextView) ;
+        mobileNumberEditText = findViewById(R.id.mobileNumber);
+        mobileNumberTextView = findViewById(R.id.mobileNumberTextView) ;
+
+        if(type.equals("google")||type.equals("signIn")){
+            userNameEditText.setVisibility(View.VISIBLE);
+            userNameTextView.setVisibility(View.VISIBLE);
+            mobileNumberEditText.setVisibility(View.VISIBLE);
+            mobileNumberTextView.setVisibility(View.VISIBLE);
+        }
+        else{
+            userName = intent.getStringExtra("tutorName") ;
+            mobileNumber = intent.getStringExtra("tutorMobileNumber") ;
+        }
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser() ;
+        myRefCandidateTutor = FirebaseDatabase.getInstance().getReference().child("CandidateTutor").child(firebaseUser.getUid()) ;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        genderBox = findViewById(R.id.gender) ;
+        currentPositionBox = findViewById(R.id.currentPosition);
+        edu_instituteNameBox = findViewById(R.id.edu_instituteName);
+        edu_tutorSubjectBox = findViewById(R.id.departmentSpinner);
+        attachedHallBox = findViewById(R.id.attachedHallSpinner) ;
+
+        addressEditText = findViewById(R.id.fullAddress) ;
+        edu_instituteNameEditText = findViewById(R.id.instituteNameEditText) ;
+        edu_tutorSubjectTextView = findViewById(R.id.departmentTextView) ;
+        attachedHallTextView = findViewById(R.id.attachedHallTextView) ;
+
+        // progressBar = findViewById(R.id.progressBar);
+
+        selectCurrentPosition() ;
+        selectInstituteName() ;
+        selectTutorSubject() ;
+    }
+
+    public void signUpCompletion(View view) {
+
+        gender = genderBox.getSelectedItem().toString().trim();
+        areaAddress = addressEditText.getText().toString().trim();
+        currentPosition = currentPositionBox.getSelectedItem().toString();
+        edu_instituteName = edu_instituteNameBox.getSelectedItem().toString();
+        edu_tutorSubject = edu_tutorSubjectBox.getSelectedItem().toString();
+        attachedHall = attachedHallBox.getText().toString();
+
+        if(gender.equals("GENDER")){
+            TextView textView = (TextView) genderBox.getSelectedView() ;
+            textView.setError("");
+            return;
+        }
+        if(currentPosition.equals("CURRENT POSITION")){
+            TextView textView = (TextView) currentPositionBox.getSelectedView() ;
+            textView.setError("");
+            return;
+        }
+        if(edu_instituteName.equals("INSTITUTE")&&!currentPosition.equals("College")){
+            TextView textView = (TextView) edu_instituteNameBox.getSelectedView() ;
+            textView.setError("");
+            return;
+        }
+        else if(currentPosition.equals("College")){
+            edu_instituteName = edu_instituteNameEditText.getText().toString() ;
+            if(edu_instituteName.equals("")){
+                edu_instituteNameEditText.setError(""); ;
+                return;
+            }
+        }
+        if(edu_tutorSubject.equals("SUBJECT/DEPARTMENT")&& !currentPosition.equals("College")){
+            TextView textView = (TextView) edu_tutorSubjectBox.getSelectedView() ;
+            textView.setError("");
+            return;
+        }
+        else if(currentPosition.equals("College")){
+            edu_tutorSubject = "!" ;
+        }
+
+        if(attachedHall.equals("")&&!currentPosition.equals("College")){
+            attachedHallBox.setError("");
+            return;
+        }
+        else if(currentPosition.equals("College")){
+            attachedHall = "!" ;
+        }
+
+        if(areaAddress.equals("")){
+            addressEditText.setError("");
+            return;
+        }
+
+        if(type.equals("google")||type.equals("signIn")){
+            userName = userNameEditText.getText().toString() ;
+            email = firebaseUser.getEmail() ;
+            mobileNumber = mobileNumberEditText.getText().toString() ;
+
+            if(userName.equals("")){
+                userNameEditText.setError("");
+                return;
+            }
+            if(mobileNumber.equals("")){
+                mobileNumberEditText.setError("");
+                return;
+            }
+
+            candidateTutorInfo = new CandidateTutorInfo(userName, email, mobileNumber, gender, areaAddress, currentPosition, edu_instituteName, edu_tutorSubject, attachedHall);
+            myRefCandidateTutor.setValue(candidateTutorInfo) ;
+            goToTutorSignUpActivityStep2();
+        }
+        else if(type.equals("signUp")){
+            email = firebaseUser.getEmail() ;
+            candidateTutorInfo = new CandidateTutorInfo(userName, email, mobileNumber, gender, areaAddress, currentPosition, edu_instituteName, edu_tutorSubject, attachedHall);
+            myRefCandidateTutor.setValue(candidateTutorInfo) ;
+            goToTutorSignUpActivityStep2();
+        }
+    }
+
+    public void goToTutorSignUpActivityStep2(){
+        Intent intent = new Intent(this, TutorSignUpActivityStep2.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void backFromSignUpT_Activity(View view){
+        Intent intent = new Intent(this, TutorSignInActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void selectCurrentPosition(){
+        currentPositionBox.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(currentPositionBox.getSelectedItemPosition()==0){
+                    currentPosition = "" ;
+                }
+                else {
+                    currentPosition = currentPositionBox.getSelectedItem().toString() ;
+                    if(currentPosition.equals("College")){
+                        edu_instituteNameEditText.setVisibility(View.VISIBLE);
+                        edu_instituteNameBox.setVisibility(View.GONE);
+                        edu_tutorSubjectBox.setVisibility(View.GONE);
+                        edu_tutorSubjectTextView.setVisibility(View.GONE);
+                        attachedHallBox.setVisibility(View.GONE);
+                        attachedHallTextView.setVisibility(View.GONE);
+                        edu_tutorSubject = "" ;
+                        attachedHall = "" ;
+
+                    }
+                    else{
+                        edu_instituteNameEditText.setVisibility(View.GONE);
+                        edu_instituteNameBox.setVisibility(View.VISIBLE);
+                        edu_tutorSubjectBox.setVisibility(View.VISIBLE);
+                        edu_tutorSubjectTextView.setVisibility(View.VISIBLE);
+                        attachedHallBox.setVisibility(View.VISIBLE);
+                        attachedHallTextView.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+    public void selectInstituteName() {
+        edu_instituteNameBox.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(edu_instituteNameBox.getSelectedItemPosition()==0){
+                    edu_instituteName = "" ;
+                }
+                else {
+                    if(currentPosition.equals("College")){
+                        edu_instituteName = edu_instituteNameEditText.getText().toString() ;
+                    }
+                    else if(!currentPosition.equals("")){
+                        edu_instituteName = edu_instituteNameBox.getSelectedItem().toString() ;
+                    }
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // sometimes you need nothing here
+            }
+        });
+    }
+    public void selectTutorSubject() {
+        edu_tutorSubjectBox.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(edu_tutorSubjectBox.getSelectedItemPosition()==0){
+                    edu_tutorSubject = "" ;
+                }
+                else {
+                    edu_tutorSubject = edu_tutorSubjectBox.getSelectedItem().toString() ;
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+    }
+
+}
